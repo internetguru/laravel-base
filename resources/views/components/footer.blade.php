0eb8b807b5eade67f73bbd2faf5496e81e9e85bc
petrzpav <pavel.petrzela@internetguru.cz>
2025-10-01 13:02:17 +0200

    update Feedback examples

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,7 +6,8 @@
                 <x-ig::lang-switch />
                 <ul class="list-unstyled d-flex flx-wrap gap-2">
                     <li><x-ig::email-feedback /></li>
-                    <li><x-ig-feedback /></li>
+                    <li><livewire:ig-feedback wire:key="f1" /></li>
+                    <li><livewire:ig-feedback wire:key="f2" email="optional" title="Abcd" submit="XYZ" subject="DEF" /></li>
                 </ul>
                 <ul class="list-unstyled d-flex flx-wrap gap-2">
                     <li><x-ig::footer-copy /></li>

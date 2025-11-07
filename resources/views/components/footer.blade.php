ce384f14db0ba7906862ef9b4f9d3a7d07a8df83
petrzpav <pavel.petrzela@internetguru.cz>
2025-11-07 06:27:04 +0100

    Implement new feedback

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,23 @@
+<livewire:ig-feedback
+    id="feedback-form"
+    email="info@internetguru.io"
+    name="Internet Guru"
+/>
+<livewire:ig-feedback
+    id="feedback-form-2"
+    email="info@internetguru.io"
+    name="Internet Guru"
+    :fields="[
+        [
+            'name' => 'phone',
+            'required' => true,
+        ],
+        [
+            'name' => 'message',
+            'label' => 'Your Message',
+        ]
+    ]"
+/>
 <footer class="container-fluid footer">
     <div>
         <div class="footer-content">
@@ -5,9 +25,12 @@
             <div>
                 <x-ig::lang-switch />
                 <ul class="list-unstyled d-flex flx-wrap gap-2">
-                    <li><x-ig::email-feedback /></li>
-                    <li><livewire:ig-feedback wire:key="f1" /></li>
-                    <li><livewire:ig-feedback wire:key="f2" email="optional" title="Abcd" submit="XYZ" subject="DEF" /></li>
+                    <li><a href="Javascript:void(0)" x-data x-on:click.prevent="Livewire.dispatch('openFeedback', {id: 'feedback-form'})">
+                        {{ __('ig-feedback::layouts.modal.link_text') }}
+                    </a></li>
+                    <li><a href="Javascript:void(0)" x-data x-on:click.prevent="Livewire.dispatch('openFeedback', {id: 'feedback-form-2'})">
+                        Click here!
+                    </a></li>
                 </ul>
                 <ul class="list-unstyled d-flex flx-wrap gap-2">
                     <li><x-ig::footer-copy /></li>
@@ -16,4 +39,3 @@
         </div>
     </div>
 </footer>
-
